0ee8035668b5a2a2bdfe42508efba15bafb4b42c
Jan200101 <sentrycraft123@gmail.com>
2019-09-23 12:53:45 +0200

    add comments to example

--- examples/toggle_lights.py
+++ examples/toggle_lights.py
@@ -1,6 +1,9 @@
 import ShellyPy
 
-device = ShellyPy.Shelly("192.168.0.5") # try connecting to the Shelly device under that ip
+# try connecting to the Shelly device under that ip
+device = ShellyPy.Shelly("192.168.0.5")
+# WILL throw an exception if the device is not reachable, gives a bad response or requires a login
 
 device.relay(0, turn=True) # turn the relay at index 0 on
 device.relay(0, turn=False) # same as bove but turn it off
+# Shelly 1 devices only have 1 relay
