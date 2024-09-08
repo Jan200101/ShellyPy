30387ace2e6a6393fa69aee3306f294d9fc03453
Sebastian Wolf <Sebastian.Wolf@pace-systems.de>
2024-09-08 20:37:21 +0200

    Remove python2 from setup

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setuptools.setup(
     install_requires=requirements,
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
